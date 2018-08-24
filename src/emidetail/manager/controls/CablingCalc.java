e94b64fc5da5c9f396dc66b368290be67f833946
denis.vitovtov <denis.vitovtov@datcroft.com>
2018-08-24 23:32:33 +0400

    Init calculate cabling form and method

---
+++ src/emidetail/manager/controls/CablingCalc.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emidetail.manager.controls;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DVAmod
+ */
+public class CablingCalc {
+    
+    private ArrayList<Float> diametr;
+    private ArrayList<Integer> count;
+    private ArrayList<Integer> type;
+    
+    private Float width;
+    private Float height;
+    
+    public Integer trayWidth;
+    public Integer trayHeight;
+    
+    public void calculateDiametr() {
+        
+    }
+    
+    private void calcFiltrTray() {
+        
+    }
+    
+}
